72ad2b966f802625503d2f73e4b6eadab5406535
Irene Joy Cahilo <irene.cahilo@crmworks.asia>
2016-06-03 15:27:39 +0800

    VCRM-26 Sprint 2
    Contact's record view

---
+++ custom/modules/Contacts/clients/base/views/record/record.php
@@ -0,0 +1,227 @@
+<?php
+$viewdefs['Contacts'] = 
+array (
+  'base' => 
+  array (
+    'view' => 
+    array (
+      'record' => 
+      array (
+        'buttons' => 
+        array (
+          0 => 
+          array (
+            'type' => 'button',
+            'name' => 'cancel_button',
+            'label' => 'LBL_CANCEL_BUTTON_LABEL',
+            'css_class' => 'btn-invisible btn-link',
+            'showOn' => 'edit',
+            'events' => 
+            array (
+              'click' => 'button:cancel_button:click',
+            ),
+          ),
+          1 => 
+          array (
+            'type' => 'rowaction',
+            'event' => 'button:save_button:click',
+            'name' => 'save_button',
+            'label' => 'LBL_SAVE_BUTTON_LABEL',
+            'css_class' => 'btn btn-primary',
+            'showOn' => 'edit',
+            'acl_action' => 'edit',
+          ),
+          2 => 
+          array (
+            'type' => 'actiondropdown',
+            'name' => 'main_dropdown',
+            'primary' => true,
+            'showOn' => 'view',
+            'buttons' => 
+            array (
+              0 => 
+              array (
+                'type' => 'rowaction',
+                'event' => 'button:edit_button:click',
+                'name' => 'edit_button',
+                'label' => 'LBL_EDIT_BUTTON_LABEL',
+                'acl_action' => 'edit',
+              ),
+              1 => 
+              array (
+                'type' => 'shareaction',
+                'name' => 'share',
+                'label' => 'LBL_RECORD_SHARE_BUTTON',
+                'acl_action' => 'view',
+              ),
+              2 => 
+              array (
+                'type' => 'pdfaction',
+                'name' => 'download-pdf',
+                'label' => 'LBL_PDF_VIEW',
+                'action' => 'download',
+                'acl_action' => 'view',
+              ),
+              3 => 
+              array (
+                'type' => 'pdfaction',
+                'name' => 'email-pdf',
+                'label' => 'LBL_PDF_EMAIL',
+                'action' => 'email',
+                'acl_action' => 'view',
+              ),
+              4 => 
+              array (
+                'type' => 'divider',
+              ),
+              5 => 
+              array (
+                'type' => 'manage-subscription',
+                'name' => 'manage_subscription_button',
+                'label' => 'LBL_MANAGE_SUBSCRIPTIONS',
+                'showOn' => 'view',
+                'value' => 'edit',
+              ),
+              6 => 
+              array (
+                'type' => 'vcard',
+                'name' => 'vcard_button',
+                'label' => 'LBL_VCARD_DOWNLOAD',
+                'acl_action' => 'edit',
+              ),
+              7 => 
+              array (
+                'type' => 'divider',
+              ),
+              8 => 
+              array (
+                'type' => 'rowaction',
+                'event' => 'button:find_duplicates_button:click',
+                'name' => 'find_duplicates',
+                'label' => 'LBL_DUP_MERGE',
+                'acl_action' => 'edit',
+              ),
+              9 => 
+              array (
+                'type' => 'rowaction',
+                'event' => 'button:duplicate_button:click',
+                'name' => 'duplicate_button',
+                'label' => 'LBL_DUPLICATE_BUTTON_LABEL',
+                'acl_module' => 'Contacts',
+                'acl_action' => 'create',
+              ),
+              10 => 
+              array (
+                'type' => 'rowaction',
+                'event' => 'button:historical_summary_button:click',
+                'name' => 'historical_summary_button',
+                'label' => 'LBL_HISTORICAL_SUMMARY',
+                'acl_action' => 'view',
+              ),
+              11 => 
+              array (
+                'type' => 'rowaction',
+                'event' => 'button:audit_button:click',
+                'name' => 'audit_button',
+                'label' => 'LNK_VIEW_CHANGE_LOG',
+                'acl_action' => 'view',
+              ),
+              12 => 
+              array (
+                'type' => 'divider',
+              ),
+              13 => 
+              array (
+                'type' => 'rowaction',
+                'event' => 'button:delete_button:click',
+                'name' => 'delete_button',
+                'label' => 'LBL_DELETE_BUTTON_LABEL',
+                'acl_action' => 'delete',
+              ),
+            ),
+          ),
+          3 => 
+          array (
+            'name' => 'sidebar_toggle',
+            'type' => 'sidebartoggle',
+          ),
+        ),
+        'panels' => 
+        array (
+          0 => 
+          array (
+            'name' => 'panel_header',
+            'header' => true,
+            'fields' => 
+            array (
+              0 => 
+              array (
+                'name' => 'picture',
+                'type' => 'avatar',
+                'size' => 'large',
+                'dismiss_label' => true,
+              ),
+              1 => 
+              array (
+                'name' => 'full_name',
+                'label' => 'LBL_NAME',
+                'dismiss_label' => true,
+                'type' => 'fullname',
+                'fields' => 
+                array (
+                  0 => 'salutation',
+                  1 => 'first_name',
+                  2 => 'last_name',
+                ),
+              ),
+              2 => 
+              array (
+                'name' => 'favorite',
+                'label' => 'LBL_FAVORITE',
+                'type' => 'favorite',
+                'dismiss_label' => true,
+              ),
+              3 => 
+              array (
+                'name' => 'follow',
+                'label' => 'LBL_FOLLOW',
+                'type' => 'follow',
+                'readonly' => true,
+                'dismiss_label' => true,
+              ),
+            ),
+          ),
+          1 => 
+          array (
+            'name' => 'panel_body',
+            'label' => 'LBL_RECORD_BODY',
+            'columns' => 2,
+            'labelsOnTop' => true,
+            'placeholders' => true,
+            'newTab' => false,
+            'panelDefault' => 'expanded',
+            'fields' => 
+            array (
+              0 => 'title',
+              1 => 'phone_mobile',
+              2 => 'department',
+              3 => 'do_not_call',
+              4 => 'account_name',
+              5 => 'email',
+              6 => 'phone_fax',
+              7 => 'phone_work',
+              8 => 'report_to_name',
+              9 => 'sync_contact',
+              10 => 'lead_source',
+              11 => 'assigned_user_name',
+            ),
+          ),
+        ),
+        'templateMeta' => 
+        array (
+          'useTabs' => false,
+        ),
+      ),
+    ),
+  ),
+);
